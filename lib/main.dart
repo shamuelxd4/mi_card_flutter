01049ea5d34f4662f97b911ff7514b642a93f5b6
Samuel Quenta Carvajal <50058913+shamuelxd4@users.noreply.github.com>
2021-02-23 22:50:51 -0400

    complete tarea1

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,73 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text(
+                "Tarea1 Angela",
+              ),
+            ),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          backgroundColor: Colors.blueGrey[800],
+          body: RowColors(),
+        ),
+      ),
+    );
+  }
+}
+
+class RowColors extends StatelessWidget {
+  const RowColors({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          color: Colors.red,
+          width: 100,
+        ),
+        Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.yellow,
+                height: 100,
+                width: 100,
+              ),
+              Container(
+                color: Colors.lime[900],
+                height: 100,
+                width: 100,
+              ),
+            ],
+          ),
+        ),
+        Container(
+          color: Colors.green[900],
+          width: 100,
+        ),
+      ],
+    );
+  }
 }
